6af05a6231786b1c7f47c622d6f3b3bf330b17e3
lemichael <mail@michael-le.net>
2015-04-27 00:38:42 +0200

    Initial Commit

---
+++ src/main/java/com/walletcoach/walletcoach/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.walletcoach.walletcoach;
+
+/**
+ * GitHub integration demo
+ * @author Michael Le
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Welcome to WalletCoach!");
+    }
+}
