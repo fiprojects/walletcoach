12fa53327320b044ed0ca59c57c724d0ff4b0f5e
lemichael <mail@michael-le.net>
2015-06-15 22:16:41 +0200

    Tools Refactor

--- src/main/java/com/walletcoach/walletcoach/tools/Piechart.java
+++ src/main/java/com/walletcoach/walletcoach/tools/Piechart.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.walletcoach.walletcoach.tools;
 
 
@@ -49,9 +44,4 @@ public class Piechart {
         point = new Point(x,y);
         return point;
     }
-    
-    
-    
-    
-    
 }
